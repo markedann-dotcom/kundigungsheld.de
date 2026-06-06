9ff519bc2c1545cd8988b17fc167f35976841896
markedann-dotcom <markedann@gmail.com>
2026-06-07 00:08:54 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,6 @@
 import { Button as ButtonPrimitive } from '@base-ui/react/button'
 import { cva, type VariantProps } from 'class-variance-authority'
+import { Slot } from '@radix-ui/react-slot'
 
 import { cn } from '@/lib/utils'
 
@@ -40,16 +41,24 @@ const buttonVariants = cva(
   },
 )
 
+export interface ButtonProps extends ButtonPrimitive.Props, VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
 function Button({
   className,
   variant = 'default',
   size = 'default',
+  asChild = false,
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonProps) {
+  const renderElement = asChild ? <Slot /> : undefined;
+
   return (
     <ButtonPrimitive
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      render={renderElement}
       {...props}
     />
   )
